4487db2c88fa21d8642f92a3292a354986ff85d5
benfliu <ben.franklin.liu@gmail.com>
2023-11-05 12:06:57 -0600

    finishing up

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios, { all } from 'axios';
 import './home.css'
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 const companyNames = [
-    'Apple',
     'Microsoft',
     'Google',
     'Amazon',
@@ -14,14 +13,24 @@ const companyNames = [
     'Goldman Sachs'
   ];
 
+const metrics = ["Close", "Revenues", "EBIT", "Gross Profit", "Basic EPS", "Cost of Revenue",
+                 "Total Assets", "Total Liabilities", "Stockholder's Equity",
+                 "Operating Cash Flow", "Free Cash Flow", "Volume"];
+
 function Home() {
   const [companyName, setCompanyName] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [combinedData, setCombinedData] = useState({
     labels: [],
     datasets: []
   });
+  const [metric, setMetric] = useState(metrics[0]);
+
+  const handleMetricChange = (event) => {
+    setMetric(event.target.value);
+  };
 
   const handleInputChange = (event) => {
     const inputValue = event.target.value;
@@ -41,7 +50,7 @@ function Home() {
 
 const handleKeyPress = (event) => {
     // It will trigger by pressing the Enter key
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && (isSubmitted === false || isLoading === false)) {
         runAnalysis();
     }
 };
@@ -49,21 +58,27 @@ const handleKeyPress = (event) => {
 const runAnalysis = async () => {
     try {
         setIsSubmitted(true);
+        setIsLoading(true);
         const response = await axios.get(`/get_articles_by_quarter?company_name=${encodeURIComponent(companyName)}`);
         console.log(response.data);
-        const all_clean_data = response.data;
-        generateGraph("Close", all_clean_data);
-        // Assuming the response.data is the array of articles
+        const allCleanData = response.data;
+        generateGraph(metric,allCleanData).then(() => {
+            setIsLoading(false);
+          }).catch((error) => {
+            console.error('An error occurred:', error);
+            setIsLoading(false);
+          });
     } catch (error) {
         console.error('Error fetching articles:', error);
+        setIsLoading(false);
         // Handle error appropriately
     }
 };
 
-    const generateGraph = async (metric, all_clean_data) => {
+    const generateGraph = async (metric, allCleanData) => {
         try {
-            const data = all_clean_data["data"];
-            const predictions = all_clean_data["predictions"];
+            const data = allCleanData["data"];
+            const predictions = allCleanData["predictions"];
             console.log(data['x_vals']);
             console.log(predictions['x_vals']);
             const allQuarters = data["x_vals"].concat(predictions["x_vals"]);
@@ -77,12 +92,12 @@ const runAnalysis = async () => {
                   },
                 ],
               };
-              
+            const arr = Array(allQuarters.length-3).fill(null).concat([data['vals'][data['vals'].length-1]]);
             const dataFromDataFrame2 = {
             datasets: [
-                {
+                { 
                 label: predictions['y_label'],
-                data: predictions['vals'], // This should be your actual data
+                data: arr.concat(predictions['vals']), // This should be your actual data
                 borderColor: 'rgb(54, 162, 235)', // Color for Dataset 2
                 backgroundColor: 'rgba(54, 162, 235, 0.5)',
                 },
@@ -100,6 +115,7 @@ const runAnalysis = async () => {
   return (
     <div className="homepage">
         <div className={`search-container ${isSubmitted ? 'at-top' : 'centered'}`}>
+            <img className="logo" src="/context-ai-logo.png" alt="Context AI Logo" />
             <h1 className="search-heading">Input a company name below!</h1>
             <input
             type="text"
@@ -116,7 +132,16 @@ const runAnalysis = async () => {
                 </div>
                 ))}
             </div>)}
+            <div className={`metric-selector ${isSubmitted ? '' : 'hidden'}`}>
+                <label htmlFor="metric-select">Choose a metric: </label>
+                <select id="metric-select" value={metric} onChange={handleMetricChange}>
+                {metrics.map((m, index) => (
+                    <option key={index} value={m}>{m}</option>
+                ))}
+                </select>
+            </div>
         </div>
+        <div id="loadingSpinner" className={`spinner ${isLoading ? '' : 'hidden'}`}></div>
         <Line data={combinedData}/>
         {/* {quarterToArticles.length > 0 && (
         <div>
